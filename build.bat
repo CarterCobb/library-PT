4817ef9e1aa65ad689816eab7affca3d72671823
CarterCobb <carter.cobb72@gmail.com>
2022-05-20 18:44:54 -0600

    updated build stages

--- build.bat
+++ build.bat
@@ -4,9 +4,12 @@ set GOOS=linux
 echo Building...
 
 @REM Add all the lambdas below to build binaries 
+go build -o build/user lambdas/user.go
 go build -o build/book lambdas/book.go
+go build -o build/book_actions lambdas/book_actions.go
+go build -o build/authorizer lambdas/authorizer.go
 
-echo Deploying...
-serverless deploy
+@REM echo Deploying...
+@REM serverless deploy
 
 pause
