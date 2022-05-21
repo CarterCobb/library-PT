d7152e883a7f06065cae8b59056644f95d9240c5
CarterCobb <carter.cobb72@gmail.com>
2022-05-21 15:00:52 -0600

    added book management page

---
+++ client/src/pages/BookManagement.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import BookAPI from "../api/book";
+import { connect } from "react-redux";
+import { mapStateToProps } from "../redux/reducer";
+
+class BookManagement extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
+
+  componentDidMount() {
+    BookAPI.getAllBooks((books, err) => {
+      if (err) console.log(err);
+      else this.setState({ books });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Book Management</h1>
+        <button onClick={() => (window.location.href = "/")}>Home</button>
+        <h3>Books:</h3>
+        <pre>{JSON.stringify(this.state.books, null, 2)}</pre>
+        <h3>Logged in user:</h3>
+        <pre>{JSON.stringify(this.props.user, null, 2)}</pre>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(BookManagement);
