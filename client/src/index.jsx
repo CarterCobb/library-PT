ac6a176a835209e83d680f6ac57b931ef425f873
CarterCobb <carter.cobb72@gmail.com>
2022-05-21 15:00:31 -0600

    added error boundary & redux store

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import ErrorBoundary from "./code-spliting/error-boundary";
+import { legacy_createStore } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./redux/reducer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = legacy_createStore(reducer);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
-
