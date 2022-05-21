7442e8a130b74454f938435bc663280e76c88cd6
CarterCobb <carter.cobb72@gmail.com>
2022-05-20 23:35:10 -0600

    full authorizer

--- lambdas/authorizer.go
+++ lambdas/authorizer.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	"log"
+	"cartercobb/m/pkg/user"
 	"context"
 	"errors"
-	"strings"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/golang-jwt/jwt"
+	"strings"
 )
 
-// Help function to generate an IAM policy
-func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+var (
+	dynaClient dynamodbiface.DynamoDBAPI
+)
+
+const jwtSecret = "jysyVggrzwwioncbTAGckMSGsyZizuXtlSTkyKojvtDSWYLDCTeRkpjaInxBvJtHxAKtSvRYuSTJrvPQceMwcUPpBAKKnjLnQvFI"
+
+func generatePolicy(principalId, uid string, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
 	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalId}
 
 	if effect != "" && resource != "" {
@@ -26,30 +37,48 @@ func generatePolicy(principalId, effect, resource string) events.APIGatewayCusto
 		}
 	}
 
-	// Optional output with custom properties of the String, Number or Boolean type.
 	authResponse.Context = map[string]interface{}{
-		"stringKey":  "stringval",
-		"numberKey":  123,
-		"booleanKey": true,
+		"uid": uid,
 	}
 	return authResponse
 }
 
 func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	token := event.AuthorizationToken
-	log.Printf("TOKEN: %s", token)
-	switch strings.ToLower(token) {
-	case "allow":
-		return generatePolicy("user", "Allow", event.MethodArn), nil
-	case "deny":
-		return generatePolicy("user", "Deny", event.MethodArn), nil
-	case "unauthorized":
-		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized: " + token) // Return a 401 Unauthorized response
-	default:
-		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token; " + token)
+	token := strings.Split(event.AuthorizationToken, " ")[1]
+	token_parsed, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(jwtSecret), nil
+	})
+
+	if err != nil {
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unexpected error:" + err.Error())
+	}
+
+	if claims, ok := token_parsed.Claims.(jwt.MapClaims); ok && token_parsed.Valid {
+		result, err := user.FetchUser(claims["uid"].(string), "users", dynaClient)
+		if err != nil {
+			return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unexpected error:" + err.Error())
+		}
+		if result == nil {
+			return generatePolicy("user", result.UID, "Deny", event.MethodArn), nil
+		} else {
+			return generatePolicy("user", result.UID, "Allow", event.MethodArn), nil
+		}
+	} else {
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
 	}
 }
 
 func main() {
+	region := "us-west-1"
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)},
+	)
+	if err != nil {
+		return
+	}
+	dynaClient = dynamodb.New(awsSession)
 	lambda.Start(handleRequest)
 }
