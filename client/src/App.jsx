fc2533b8bea02bc564670b0e60726019babd7cf0
CarterCobb <carter.cobb72@gmail.com>
2022-05-21 15:00:42 -0600

    added routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,31 +2,39 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SecureLS from "secure-ls";
 
-// Error Boundary
-import ErrorBoundary from "./code-spliting/error-boundary";
-
 // Fallback UI
 import { fallback } from "./code-spliting/fallback-ui";
 
+//Protected route
+import ProtectedRoute from "./components/ProtectedRoute";
+
 // Pages
 const Home = lazy(() => import("./pages/Home"));
+const BookManagement = lazy(() => import("./pages/BookManagement"));
+const NotAuthorized = lazy(() => import("./pages/NotAuthorized"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 
 // Local Storage export
 export const ls = new SecureLS({ encodingType: "aes" });
 
+// API Endpoint
+export const api = "https://bebm75gygc.execute-api.us-west-1.amazonaws.com/v1";
+
 const App = () => {
   return (
-    <ErrorBoundary>
-      <Router>
-        <Suspense fallback={fallback}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Suspense>
-      </Router>
-    </ErrorBoundary>
+    <Router>
+      <Suspense fallback={fallback}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/book-management"
+            element={<ProtectedRoute element={BookManagement} />}
+          />
+          <Route path="/not-authorized" element={<NotAuthorized />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 };
 
